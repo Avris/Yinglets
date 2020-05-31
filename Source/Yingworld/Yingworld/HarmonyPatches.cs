a232061814351bb5fc343f0f9aa6120ab8974461
Brennen Cramer <48333768+BrennenCramer@users.noreply.github.com>
2020-05-31 08:55:25 -0700

    Added color sync for the female tail

---
+++ Source/Yingworld/Yingworld/HarmonyPatches.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reflection;
+using AlienRace;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+using UnityEngine;
+
+namespace Yingworld
+{
+    [HarmonyPatch(typeof(GenSpawn))]
+    internal static class HarmonyPatches
+    {
+        [HarmonyPatch(nameof(GenSpawn.Spawn), new Type[] { typeof(Thing), typeof(IntVec3), typeof(Map), typeof(Rot4), typeof(WipeMode), typeof(bool) })]
+        [HarmonyPostfix]
+        static void SpawnPostfix(ref Thing __result, Thing newThing, IntVec3 loc, Map map, Rot4 rot, WipeMode wipeMode = WipeMode.Vanish, bool respawningAfterLoad = false)
+        {
+            Pawn pawn = __result as Pawn;
+            if (pawn != null && pawn.def == YingDefOf.Alien_Yinglet)
+            {
+                AlienPartGenerator.AlienComp comp = pawn.GetComp<AlienPartGenerator.AlienComp>();
+                if (comp != null)
+                {
+                    comp.ColorChannels.TryGetValue("tail").first = comp.skinColor;
+                    comp.ColorChannels.TryGetValue("tail").second = pawn.story.hairColor;
+                }
+            }
+        }
+    }
+
+    [StaticConstructorOnStartup]
+    internal static class YingworldPatches
+    {
+        static YingworldPatches()
+        {
+            Harmony harmonyInstance = new Harmony("com.yingworld.mod");
+            harmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
