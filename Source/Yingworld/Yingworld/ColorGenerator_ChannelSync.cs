421b0ef703431eef5601ca6ad7e9291c6d57c270
Avris <avristed@gmail.com>
2020-08-13 02:51:38 +0600

    Revert "Pruning unnecessary files."
    This reverts commit 0f94ee7bdf8f3c5ac66193e56cc929407a317978.

---
+++ Source/Yingworld/Yingworld/ColorGenerator_ChannelSync.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+using AlienRace;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+using UnityEngine;
+
+namespace Yingworld
+{
+    class ColorGenerator_ChannelSync : ColorGenerator
+	{
+		public string channel;
+		public bool second;
+
+		public override Color NewRandomizedColor()
+		{
+			return new Color(1f, 0f, 1f, 1f);
+		}
+	}
+}
