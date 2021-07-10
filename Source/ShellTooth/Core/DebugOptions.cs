9d36d13644517999abfe9585a8308a14483e5fdd
Avris <avristed@gmail.com>
2021-07-10 09:03:55 -0400

    1.3 compatibility unfuck and etc
    Possibly has previously-uncommitted changes.

--- Source/ShellTooth/Core/DebugOptions.cs
+++ Source/ShellTooth/Core/DebugOptions.cs
@@ -13,6 +13,41 @@ namespace ShellTooth
         {
             Find.WindowStack.Add(new Dialog_SpawnYingletObject());
         }
+        [DebugAction("Yinglets", "Tiplod pregnancy tester", allowedGameStates = AllowedGameStates.PlayingOnMap)]
+        private static void TiplodTester()
+        {
+            Pawn mother = null;
+            DebugTool tool2 = new DebugTool("Select the father...", delegate ()
+            {
+                if (mother == null)
+                { 
+                    Log.Error("It didn't set for some reason");
+                }
+                else
+                {
+                    IntVec3 from1 = UI.MouseCell();
+                    Pawn father = from1.GetFirstPawn(Find.CurrentMap);
+                    Hediff_Pregnant hediff_Pregnant = (Hediff_Pregnant)HediffMaker.MakeHediff(HediffDefOf.Pregnant, mother, null);
+                    hediff_Pregnant.father = father;
+                    mother.health.AddHediff(hediff_Pregnant, null, null, null);
+                    bool work = false;
+                    if (mother.health.hediffSet.HasHediff(HediffDefOf.Pregnant, false))
+                    { work = true; };
+                    Log.Error($"Tried to make {mother.NameShortColored} pregnant from {father.NameShortColored}. It {(work ? "worked!" : "did not work.")}");
+                    DebugTools.curTool = null;
+                }
+            });
+            DebugTool tool1 = new DebugTool("Select the mother...", delegate ()
+            {
+                IntVec3 from2 = UI.MouseCell();
+                mother = from2.GetFirstPawn(Find.CurrentMap);
+                if (mother != null)
+                {
+                    DebugTools.curTool = tool2;
+                }
+            });
+            DebugTools.curTool = tool1;
+		}
         private static void WindowTemplate()
         {
         }
